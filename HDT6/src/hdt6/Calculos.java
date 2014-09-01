246bfa8231dbc1e62db7beccc4fae1967120c064
jho1213gt <jho1213gt@gmail.com>
2014-09-01 17:37:55 -0600

    Cambio 1.0.3

--- HDT6/src/hdt6/Calculos.java
+++ HDT6/src/hdt6/Calculos.java
@@ -16,7 +16,7 @@ import java.util.LinkedHashSet;
 import java.util.Arrays;
 
 public class Calculos {
-    Set DevJava, DevWeb, DevPhone, convergence, SET;
+    Set DevJava, DevWeb, DevPhone, subconjunto ,Convergence, SET;
     
     public void viewer(Set SET){
         String[] array=new String[SET.size()];
@@ -27,62 +27,44 @@ public class Calculos {
     }
     
     public void INDatos(String nombre, String tipo){
-        if(tipo.equals("J")){
-            DevJava.add(nombre);
-            System.out.println("Interseccion entre Java y Web de desarrolladores es: ");
-        }
-        if(tipo.equals("W")){
-            DevWeb.add(nombre);
-        }
-        if(tipo.equals("C")){
-            DevPhone.add(nombre);
-        }
-        else{
-            System.out.println("Ingrese de nuevo");
+        switch(tipo){
+            case "J":
+                DevJava.add(nombre);
+                break;
+            case "W":
+                DevWeb.add(nombre);
+                break;
+            case "C":
+                DevPhone.add(nombre);
+                break;
         }
     }
     
     public void Convergence(int counter){
         if(counter==1){
-            convergence = DevJava;
-            convergence.retainAll(DevWeb);
+            Convergence = DevJava;
+            Convergence.retainAll(DevWeb);
             System.out.println("Interseccion entre Java y Web de desarrolladores es: ");
-            String[] array=new String[SET.size()];
-            array=(String[])SET.toArray(new String[0]);
-            for(int i=0;i<SET.size();i++){
-                System.out.println(array[i]);
-            }            
+            viewer(Convergence);      
         }
         if(counter==2){
-            convergence = DevJava;
-            convergence.retainAll(DevPhone);
+            Convergence = DevJava;
+            Convergence.retainAll(DevPhone);
             System.out.println("Interseccion entre Java y Celular de desarrolladores es: ");
-            String[] array=new String[SET.size()];
-            array=(String[])SET.toArray(new String[0]);
-            for(int i=0;i<SET.size();i++){
-                System.out.println(array[i]);
-            }            
+            viewer(Convergence);
         }
         if(counter==3){
-            convergence = DevPhone;
-            convergence.retainAll(DevWeb);
+            Convergence = DevPhone;
+            Convergence.retainAll(DevWeb);
             System.out.println("Interseccion entre Celulares y Web de desarrolladores es: ");
-            String[] array=new String[SET.size()];
-            array=(String[])SET.toArray(new String[0]);
-            for(int i=0;i<SET.size();i++){
-                System.out.println(array[i]);
-            }            
+            viewer(Convergence);
         }        
         if(counter==4){
-            convergence = DevJava;
-            convergence.retainAll(DevWeb);
-            convergence.retainAll(DevPhone);
+            Convergence = DevJava;
+            Convergence.retainAll(DevWeb);
+            Convergence.retainAll(DevPhone);
             System.out.println("Interseccion entre 3 areas de desarrolladores es: ");
-            String[] array=new String[SET.size()];
-            array=(String[])SET.toArray(new String[0]);
-            for(int i=0;i<SET.size();i++){
-                System.out.println(array[i]);
-            }
+            viewer(Convergence);
         }
     }
     
@@ -104,6 +86,19 @@ public class Calculos {
                 System.out.println("Los desarrolladores de Web ocupa mayor tamanio de conjunto");    
                 viewer(DevWeb);
             }
+        }
+    }
+    
+    public void SUB(){
+        boolean space;
+        subconjunto = DevJava;
+        subconjunto.removeAll(DevWeb);
+        space = subconjunto.isEmpty();
+        
+        if(space){
+            System.out.println("El conjunto de los desarrolladores de Java son subconjunto de parte de Web");
+        }else{
+            System.out.println("El conjunto de los desarrolladores de Java NO son subconjunto de parte de Web");    
         }
     }
 }
